892f9913338af7d41522bddeda4db195fd125ca0
Andrew Snodgrass <git@pointlander.info>
2022-03-02 12:43:46 -0600

    Improvements

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"runtime"
 
 	"github.com/mjibson/go-dsp/fft"
 	"gonum.org/v1/gonum/integrate/quad"
@@ -21,94 +22,185 @@ const (
 	// d is the number of dimensions
 	d = 2
 	// N is the number of points in the Worldline
-	N = 1024
+	N = 32 * 1024
+	// Loops is the number of loops
+	Loops = 100
 )
 
-func W(a float64, worldline int64, x float64) float64 {
-	rnd := rand.New(rand.NewSource(worldline))
-	y := make([][]complex128, 0, d)
-	for i := 0; i < d; i++ {
-		y = append(y, make([]complex128, 0, N))
-		y[i] = append(y[i], 0)
-	}
-	factor := math.Sqrt(2)
-	for i := 0; i < d; i++ {
-		for j := 1; j < N; j++ {
-			y[i] = append(y[i], complex(rnd.Float64()*factor, rnd.Float64()*factor))
+var (
+	// CPUs is the number of CPUs
+	CPUs = runtime.NumCPU()
+)
+
+func MakeLoops() [][][d]float64 {
+	loops := make([][][d]float64, Loops)
+	for loop := 0; loop < Loops; loop++ {
+		rnd := rand.New(rand.NewSource(int64(loop + 1)))
+		y := make([][]complex128, 0, d)
+		for i := 0; i < d; i++ {
+			y = append(y, make([]complex128, 0, N))
+			y[i] = append(y[i], 0)
+		}
+		factor := math.Sqrt(2)
+		for i := 0; i < d; i++ {
+			for j := 1; j < N; j++ {
+				y[i] = append(y[i], complex(rnd.Float64()*factor, rnd.Float64()*factor))
+			}
+		}
+
+		yt := make([][]complex128, 0, d)
+		for i := 0; i < d; i++ {
+			yt = append(yt, fft.FFT(y[i]))
+		}
+
+		var min [d]float64
+		var max [d]float64
+		for i := 0; i < d; i++ {
+			min[i] = math.MaxFloat64
+			max[i] = -math.MaxFloat64
+		}
+		for i := 0; i < N; i++ {
+			for j := 0; j < d; j++ {
+				r := real(yt[j][i])
+				if r < min[j] {
+					min[j] = r
+				}
+				if r > max[j] {
+					max[j] = r
+				}
+			}
+		}
+
+		var norm [d]float64
+		for i := 0; i < d; i++ {
+			norm[i] = math.Abs(max[i] - min[i])
+		}
+
+		for i := 0; i < N; i++ {
+			var point [d]float64
+			for j := 0; j < d; j++ {
+				point[j] = real(yt[j][i]) / norm[0]
+			}
+			loops[loop] = append(loops[loop], point)
 		}
 	}
 
-	yt := make([][]complex128, 0, d)
-	for i := 0; i < d; i++ {
-		yt = append(yt, fft.FFT(y[i]))
-	}
+	return loops
+}
 
-	var min [d]float64
-	var max [d]float64
-	for i := 0; i < d; i++ {
-		min[i] = math.MaxFloat64
-		max[i] = -math.MaxFloat64
-	}
-	for i := 1; i < N; i++ {
-		for j := 0; j < d; j++ {
-			r := real(yt[j][i])
-			if r < min[j] {
-				min[j] = r
-			}
-			if r > max[j] {
-				max[j] = r
-			}
-		}
-	}
-
-	var norm [d]float64
-	for i := 0; i < d; i++ {
-		norm[i] = math.Abs(max[i] - min[i])
-	}
-
+func W(a float64, loop [][d]float64, x float64) (float64, float64) {
 	intersections := 0.0
-	previous := real(yt[0][0])/norm[0] + x
+	scale := a
 	a /= 2
+	var previous [d]float64
+	for i := 0; i < d; i++ {
+		previous[i] = scale*loop[0][0] + x
+	}
+	length := 0.0
 	for i := 1; i < N; i++ {
-		x := real(yt[0][i])/norm[0] + x
-		if previous < -a && x > a ||
-			previous > a && x < -a {
+		var xx [2]float64
+		for j := 0; j < d; j++ {
+			xx[j] = scale*loop[i][j] + x
+		}
+		for j := 0; j < d; j++ {
+			diff := xx[j] - previous[j]
+			length += diff * diff
+		}
+		if previous[0] < -a && xx[0] > a ||
+			previous[0] > a && xx[0] < -a {
 			intersections += 2
-		} else if previous < a && x > a ||
-			previous > a && x < a ||
-			previous < -a && x > -a ||
-			previous > -a && x < -a {
+		} else if previous[0] < a && xx[0] > a ||
+			previous[0] > a && xx[0] < a ||
+			previous[0] < -a && xx[0] > -a ||
+			previous[0] > -a && xx[0] < -a {
 			intersections++
 		}
-		previous = x
+		previous = xx
 	}
 
-	return intersections
+	return intersections, length / 4
 }
 
 func main() {
+	fmt.Println("making loops...")
+	loops := MakeLoops()
+	fmt.Println("simulating...")
+	type Result struct {
+		Intersections float64
+		Length        float64
+	}
 	points := make(plotter.XYs, 0, 10)
-	for a := 1.5; a >= .1; a -= .1 {
+	for a := 1.0; a >= .1; a -= .1 {
 		w := func(x float64) float64 {
-			sum := 0.0
-			for i := 0; i < 10; i++ {
-				sum += math.Exp(-W(a, int64(i), x))
+			done := make(chan Result, 8)
+			process := func(a float64, i int) {
+				intersections, length := W(a, loops[i], x)
+				done <- Result{
+					Intersections: intersections,
+					Length:        length,
+				}
 			}
-			return x * sum
+			sum, denominator, flight, i := 0.0, 0.0, 0, 0
+			for i < Loops && flight < CPUs {
+				go process(a, i)
+				flight++
+				i++
+			}
+			for i < Loops {
+				result := <-done
+				flight--
+				sum += math.Exp(-result.Intersections) * math.Exp(-result.Length)
+				denominator += math.Exp(-result.Length)
+
+				go process(a, i)
+				flight++
+				i++
+			}
+			for i := 0; i < flight; i++ {
+				result := <-done
+				sum += math.Exp(-result.Intersections) * math.Exp(-result.Length)
+				denominator += math.Exp(-result.Length)
+			}
+			return x * sum / denominator
 		}
-		e := quad.Fixed(w, -2, 2, 10000, nil, 0)
+		e := quad.Fixed(w, -1, 1, 1000, nil, 0)
 		fmt.Println(a, e)
 		points = append(points, plotter.XY{X: a, Y: e})
 	}
 	for a := .09; a >= .01; a -= .01 {
 		w := func(x float64) float64 {
-			sum := 0.0
-			for i := 0; i < 10; i++ {
-				sum += math.Exp(-W(a, int64(i), x))
+			done := make(chan Result, 8)
+			process := func(a float64, i int) {
+				intersections, length := W(a, loops[i], x)
+				done <- Result{
+					Intersections: intersections,
+					Length:        length,
+				}
 			}
-			return x * sum
+			sum, denominator, flight, i := 0.0, 0.0, 0, 0
+			for i < Loops && flight < CPUs {
+				go process(a, i)
+				flight++
+				i++
+			}
+			for i < Loops {
+				result := <-done
+				flight--
+				sum += math.Exp(-result.Intersections) * math.Exp(-result.Length)
+				denominator += math.Exp(-result.Length)
+
+				go process(a, i)
+				flight++
+				i++
+			}
+			for i := 0; i < flight; i++ {
+				result := <-done
+				sum += math.Exp(-result.Intersections) * math.Exp(-result.Length)
+				denominator += math.Exp(-result.Length)
+			}
+			return x * sum / denominator
 		}
-		e := quad.Fixed(w, -2, 2, 10000, nil, 0)
+		e := quad.Fixed(w, -1, 1, 1000, nil, 0)
 		fmt.Println(a, e)
 		points = append(points, plotter.XY{X: a, Y: e})
 	}
